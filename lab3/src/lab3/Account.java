e2a605c6876fb105ff31fe3332ee1b60e8b47a43
KAU <KAU@KAU-PC>
2017-03-20 17:37:18 +0900

    3 20 inheritance commit

--- lab3/src/lab3/Account.java
+++ lab3/src/lab3/Account.java
@@ -1,18 +1,26 @@
 package lab3;
 
 public class Account {
-	public double balance=0;
+	private double balance=0;
 	
+	public Account(double m) //생성자
+	{
+		balance=m;
+	}
 	public void credit(double m)
 	{
 		balance+=m;
+	}
+	public void debit(double m)
+	{
+		balance-=m;
 	}
 	public double getBalance()
 	{
 		return balance;
 	}
-	public void debit(double m)
+	protected void setBalance(double m) //잔액수정
 	{
-		balance-=m;
+		balance=m;
 	}
 }
--- lab3/src/lab3/AccountTest.java
+++ lab3/src/lab3/AccountTest.java
@@ -5,39 +5,33 @@ import java.util.Scanner;
 public class AccountTest {
 	public static void main(String args[])
 	{
-		Account a1=new Account();
-		Account a2=new Account();
+		checkingAccount a1=new checkingAccount(100.0);
+		checkingAccount a2=new checkingAccount(100.0);
 		double m1,m2;
 		Scanner input=new Scanner(System.in);
+		
+		System.out.printf("Account1 balance: $%f\n",a1.getBalance());
+		System.out.printf("Account2 balance: $%f\n",a2.getBalance());
 		
 		System.out.print("Enter deposit amout for Account1 : ");
 		m1=input.nextDouble();
 		a1.credit(m1);
 		
-		System.out.printf("Account1 balance: %f\n",a1.getBalance());
-		System.out.printf("Account2 balance: %f\n",a2.getBalance());
-		
-		System.out.print("Enter deposit amout for Account2 : ");
-		m2=input.nextDouble();
-		a2.credit(m2);
-		
-		System.out.printf("Account1 balance: %f\n",a1.getBalance());
-		System.out.printf("Account2 balance: %f\n",a2.getBalance());
-		
-		System.out.print("Enter withdrawal amout for Account1 : ");
-		m1=input.nextDouble();
-		if(a1.getBalance()>m1) a1.debit(m1);
-		else System.out.println("Cannot withdraw");
-		
-		System.out.printf("Account1 balance: %f\n",a1.getBalance());
-		System.out.printf("Account2 balance: %f\n",a2.getBalance());
+		System.out.printf("\nAccount1 balance: $%f\n",a1.getBalance());
+		System.out.printf("Account2 balance: $%f\n",a2.getBalance());
 		
 		System.out.print("Enter withdrawal amout for Account2 : ");
 		m2=input.nextDouble();
-		if(a2.getBalance()>m2) a2.debit(m2);
-		else System.out.println("Cannot withdraw");
+		a2.debit(m2);
 		
-		System.out.printf("Account1 balance: %f\n",a1.getBalance());
-		System.out.printf("Account2 balance: %f\n",a2.getBalance());
+		System.out.printf("\nAccount1 balance: $%f\n",a1.getBalance());
+		System.out.printf("Account2 balance: $%f\n",a2.getBalance());
+		
+		a1.nextMonth();
+		a2.nextMonth();
+		
+		System.out.println("\nnext month!");
+		System.out.printf("Account1 balance: $%f\n",a1.getBalance());
+		System.out.printf("Account2 balance: $%f\n",a2.getBalance());
 	}
 }
